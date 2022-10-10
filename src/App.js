aab34413165b503fece8617ad622a4e1bceceea3
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-10 23:30:26 +0200

    app styles for wider screen

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Attribution from "./Components/Attribution";
 import Background from "./Components/Background";
 
 import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
       <Background />
       <Header />
+      <Card />
       <Attribution />
     </div>
   );
