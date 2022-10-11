213cdbffc291aae8210e4643876d2b5bd408719f
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-11 11:43:12 +0200

    changing font on attribution component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,26 @@ import React from "react";
 import classes from "./Card.module.css";
 
 function Card() {
-  return <div className={classes.card}>Card</div>;
+  return (
+    <div className={classes.card}>
+      <div className={classes["card__grid"]}>
+        <div className={classes["card__views"]}>100K previews</div>
+        <input
+          className={classes["card__input"]}
+          type="range"
+          name="range"
+          id="range-input"
+          min={1}
+          max={3}
+          step={1}
+        />
+        <div className={classes["card__payment-wrapper"]}>
+          <span className={classes["card__range-value"]}>$16.00</span>
+          <span className={classes["card__per-month"]}>/ month</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
