5b46f35efd79285bda97165ee1c99a3d9b94c86e
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-11 16:08:34 +0200

    adding state to watch range value

--- src/Components/UI/Input.jsx
+++ src/Components/UI/Input.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./Input.module.css";
 
+const rangeArray = [
+  {
+    range: 1,
+    price: 5,
+  },
+  {
+    range: 2,
+    price: 16,
+  },
+  {
+    range: 3,
+    price: 30,
+  },
+];
+
 function Input() {
+  const [value, setValue] = useState(rangeArray[1].range);
+
+  const valueChangeHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+    setValue(e.target.value - 1);
+  };
+
   return (
     <input
       className={classes["card__input"]}
       type="range"
-      min={1}
-      max={3}
+      value={value.range}
+      min={0}
+      max={2}
       step={1}
+      onChange={valueChangeHandler}
     />
   );
 }
