769e45721b23736d98b193f3bcbbf04391d9bc2e
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-14 22:24:25 +0200

    creating billing elements with styles

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ import Input from "./UI/Input";
 
 const prices = [5, 16, 35];
 
-function Card() {
+const Card = () => {
   const priceChangeHandler = (index) => {
     const value = document.getElementById("price");
     value.innerHTML = `$${prices[index].toFixed(2)}`;
@@ -22,17 +22,18 @@ function Card() {
           </span>
           <span className={classes["card__per-month"]}>/ month</span>
         </div>
-        <div className={classes["billing-wrapper"]}>
-          <div>Monthly Billing</div>
-          <label className={classes.switch}>
-            <input type="checkbox" />
-            <span className={classes.slider}></span>
-          </label>
-          <div>Yearly Billing</div>
-        </div>
+      </div>
+      <div className={classes["billing-wrapper"]}>
+        <span className={classes.month}>Monthly Billing</span>
+        <label className={classes.switch}>
+          <input type="checkbox" />
+          <span className={classes.slider}></span>
+        </label>
+        <span className={classes.year}>Yearly Billing</span>
+        <div className={classes.discount}>-25% discount</div>
       </div>
     </div>
   );
-}
+};
 
 export default Card;
