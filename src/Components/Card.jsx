557d3cbba839ba577b1d955a6d1bf67398500b97
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-16 21:49:55 +0200

    removing redundant helper text

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -28,12 +28,6 @@ const Card = () => {
     }
   };
 
-  //   - 10K pageviews / $8 per month
-  // - 50K pageviews / $12 per month
-  // - 100K pageviews / $16 per month
-  // - 500k pageviews / $24 per month
-  // - 1M pageviews / $36 per month
-
   return (
     <div className={classes.card}>
       <div className={classes["card__grid"]}>
